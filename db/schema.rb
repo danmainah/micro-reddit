11d65b947d009e2fac72874cc4bb6fde361a8cf6
unknown <danielmainawaweru@gmail.com>
2021-06-15 17:13:56 +0300

    Add link between link and user models

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_15_135856) do
+ActiveRecord::Schema.define(version: 2021_06_15_141142) do
 
   create_table "links", force: :cascade do |t|
     t.string "title"
     t.string "url"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_links_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
