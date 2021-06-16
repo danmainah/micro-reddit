e83d2d1e5677c8670e62d75c0d88a79d729ac204
unknown <danielmainawaweru@gmail.com>
2021-06-16 21:42:25 +0300

    Add styling to show and index files

--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,6 @@ Rails.application.routes.draw do
    resources :comments
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root 'links#index'
+  # root 'links#index'
+  root :to => 'links#index' 
 end
